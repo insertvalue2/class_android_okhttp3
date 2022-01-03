e1c12c432fb5e6248f5bf3eb069b2065af03c89c
questzz@naver.com <questzz@naver.com>
2022-01-04 00:12:53 +0900

    retrofit and okhttp logging and header

--- app/src/main/java/com/nomadlab/myretrofit3/repository/FakeService.java
+++ app/src/main/java/com/nomadlab/myretrofit3/repository/FakeService.java
@@ -22,12 +22,17 @@ import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 import retrofit2.http.Path;
 
 public interface FakeService {
 
 
     @GET("todos")
+    @Headers({
+            "Accept: application/json",
+            "User-Agent: myapp"
+    })
     Call<ArrayList<Todo>> getTodos();
 
     @GET("todos/{id}")
